1656d43753a363fcc5a4383ffabdf1565ef6d74a
JuliaAvsyuk <julia.avsyuk@mail.ru>
2020-04-21 20:54:17 +0300

    ###--------------------------------------------------------### refactor(product): добавь класс greet c несколькими функциями
    Создали класс Cat в файле greater.js с функциями, вызвали их из основного файла index.js
    ###--------------------------------------------------------###

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import './index.scss';
-import greet from './greeter.js';
+import Cat from './greeter/greeter.js';
 
 console.log("I'm the entry point");
-greet();
+let newCat = new Cat('Bublick', 2, 'May-may');
+newCat.myName();
+newCat.myAge();
+newCat.saySmt();
+Cat.greet();
